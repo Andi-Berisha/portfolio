e64c5b0e10e0e91536f7f75cdb621854cac8e564
Andi Berisha <aberisha@qa.com>
2021-06-25 19:52:08 +0100

    fixed mail to

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -6,7 +6,7 @@ import Title from '../Title/Title';
 
 const Contact = () => {
   const { contact } = useContext(PortfolioContext);
-  const { cta, btn, email } = contact;
+  const { cta, btn } = contact;
 
   return (
     <section id="contact">
@@ -21,7 +21,7 @@ const Contact = () => {
               target="_blank"
               rel="noopener noreferrer"
               className="cta-btn cta-btn--resume"
-              href={email ? `mailto:${email}` : 'andiberisha@hotmail.co.uk'}
+              href="mailto:andiberisha@hotmail.co.uk"
             >
               {btn || "Let's Talk"}
             </a>
